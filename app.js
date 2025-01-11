323a4c8e6c4b409caffb193ca8b2b7ac4a19de60
[naeemkash] <[naeemmuhammad1995@gmail.com]>
2025-01-11 02:56:46 -0800

    new commit added

---
+++ app.js
@@ -0,0 +1,45 @@
+
+// How to print message in Js
+
+var b = "Hello SMIT Students"
+console.log("This is our 1st class of js")
+
+// Variables , var ,let, const
+var a = "Sylani ka Pakistan"
+console.log(a+ " " +b)
+
+var message1 = "<h3>Hello world in the web page<h3>"
+var message2 = "<h2>saylani ke JS ke class chal rhi hay yahan</h2>"
+var message3 = "<h1>Road sary bndh hay han. jo SMIT Agaya usko shout out</h1>"
+
+document.write(message1+ "<hr>" +message2+ "<hr>" +message3)
+
+//confirm("Do you want to proceed more?")
+//alert("Do you want to proceed more?")
+
+var input =prompt("Enter your number:")
+//alert("your input data is " + "" + input)
+//document.write("your input data is " + "" + input)
+
+document.write(input +" x 1 =" + input * 1 + "<br>")
+document.write(input +" x 2 =" + input * 2 + "<br>")
+document.write(input +" x 3 =" + input * 3 +  "<br>")
+document.write(input +" x 4 =" + input * 4 + "<br>")
+document.write(input +" x 5 =" + input * 5 + "<br>")
+document.write(input +" x 6 =" + input * 6 + "<br>")
+document.write(input +" x 7 =" + input * 7 + "<br>")
+document.write(input +" x 8 =" + input * 8 +"<br>")
+document.write(input +" x 9 =" + input * 9 +"<br>")
+document.write(input +" x 10 =" + input * 10 +"<br>")
+
+
+// Data Types
+// String , Number , Boolean, Decimal
+var str = "Generative AI" //String
+var num = 10; //Number
+var dec = 10.23; // Decimal
+var condition1 = true
+var condition2 = false
+
+console.log(condition1)
+console.log(num + dec)
